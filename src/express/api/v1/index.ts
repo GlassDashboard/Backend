bb21b1188b709db544fb7f55ef7ce9adebcfa895
Santio <admin@santio.me>
2022-10-19 18:00:34 -0400

    Remove debugging

--- src/express/api/v1/index.ts
+++ src/express/api/v1/index.ts
@@ -31,15 +31,6 @@ router.get('/test/:test', (req, res) => {
             command: 'say hey',
             original: 'say hey'
         }))
-    } else if (test == 2) {
-        io.to('santiotest').timeout(5000).emit('FETCH_PLAYERS', (err, players) => {
-            console.log(err, players)
-        })
-    } else if (test == 3) {
-        io.to('santiotest').emit('ALL_FILES', JSON.stringify({path: '/', root: false}), files => {
-            console.log(files)
-            console.log(JSON.parse(files))
-        })
     }
 
     res.json({done:true, test});
