29967f6dc555eb9a353f826c24db964d57915962
SantioMC <business@santio.me>
2023-05-23 18:09:20 -0400

    Fix formatting for metrics

--- src/interceptors/ResponseFormatInterceptor.ts
+++ src/interceptors/ResponseFormatInterceptor.ts
@@ -1,6 +1,23 @@
 import { Action, Interceptor, InterceptorInterface } from 'routing-controllers';
 
-@Interceptor()
+const cleanMongo = (data: any) => {
+	if (!data) return data;
+
+	if (Array.isArray(data)) {
+		for (const item of data) cleanMongo(item);
+		return data;
+	}
+
+	if (data.__v) delete data.__v;
+	if (data._id) {
+		data.id = data._id;
+		delete data._id;
+	}
+
+	return data;
+};
+
+@Interceptor({ priority: 1 })
 export class ResponseFormatInterceptor implements InterceptorInterface {
 	intercept(action: Action, content: any) {
 		let data;
@@ -9,18 +26,23 @@ export class ResponseFormatInterceptor implements InterceptorInterface {
 			case 'object':
 				data = JSON.parse(JSON.stringify(content));
 				break;
-			case 'string':
-				data = { message: content };
-				break;
 			default:
 				data = content;
 		}
 
-		if (!data.message) data.message = 'OK';
-		data.error = !(action.response.statusCode >= 200 && action.response.statusCode < 300);
+		// If the response is a string, just send it
+		if (typeof content === 'string') return action.response.send(content);
 
-		return {
-			...data
-		};
+		// Handle consistency fields
+		if (!data) return action.response.status(204).json({});
+		if (!data.message) data.message = 'OK';
+
+		data.error = Math.floor(action.response.statusCode / 100) !== 2;
+
+		// Cleanup mongo fields
+		cleanMongo(data);
+
+		// Send response
+		return data;
 	}
 }
--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
-import { ExpressErrorMiddlewareInterface, Middleware, UnauthorizedError } from 'routing-controllers';
+import { ExpressErrorMiddlewareInterface, HttpError, Middleware, UnauthorizedError } from 'routing-controllers';
+import * as prometheus from '@service/prometheus';
 
 @Middleware({ type: 'after' })
 export default class ErrorMiddleware implements ExpressErrorMiddlewareInterface {
 	error(error: Error, request: Request, response: Response, next: NextFunction) {
 		response.status(500);
-		if (error.name === 'AuthorizationRequiredError') response.status(401);
+		if (error instanceof HttpError) response.status(error.httpCode);
+
+		prometheus.errorCounter.inc({
+			code: response.statusCode.toString(),
+			method: request.method,
+			path: request.route.path
+		});
 
 		response.json({
 			error: true,
