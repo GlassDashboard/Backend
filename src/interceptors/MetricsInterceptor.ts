68ae8f17bd19e0678b9fdb973f24ccd6eae2a08b
SantioMC <business@santio.me>
2023-05-23 18:09:06 -0400

    Add interceptor for metrics

---
+++ src/interceptors/MetricsInterceptor.ts
@@ -0,0 +1,16 @@
+import { Action, Interceptor, InterceptorInterface } from 'routing-controllers';
+import * as prometheus from '@service/prometheus';
+
+@Interceptor({ priority: 10 })
+export class MetricsInterceptor implements InterceptorInterface {
+	intercept(action: Action, result: any) {
+		prometheus.requestCounter.inc({
+			code: action.response.statusCode.toString(),
+			method: action.request.method,
+			path: action.request.route.path
+		});
+
+		prometheus.outgoingBandwidthCounter.inc(action.response.socket.bytesWritten);
+		return result;
+	}
+}
