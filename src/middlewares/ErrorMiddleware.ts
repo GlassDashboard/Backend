eac6ae076606014a90dae079544c47c69b2fe6e6
SantioMC <business@santio.me>
2023-07-23 21:30:30 -0400

    Better error handling

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { ExpressErrorMiddlewareInterface, HttpError, Middleware, UnauthorizedError } from 'routing-controllers';
+import { ExpressErrorMiddlewareInterface, HttpError, Middleware } from 'routing-controllers';
 import * as prometheus from '@service/prometheus';
 
 @Middleware({ type: 'after' })
@@ -17,7 +17,30 @@ export default class ErrorMiddleware implements ExpressErrorMiddlewareInterface 
 		response.json({
 			error: true,
 			code: error.name,
-			message: error.message
+			message: error.message,
+			stack: error.stack ? parseStack(error.stack!) : undefined
 		});
 	}
 }
+
+// Formats a stack trace into a more readable format
+const parseStack = (stack: string): string[] => {
+	return stack
+		.split('\n')
+		.filter((l) => l.startsWith('    at '))
+		.map((l) => l.substring(7))
+		.map((l) => {
+			const parts = l.split(' ');
+			const file = parts.pop()?.replace('(', '').replace(')', '') || '<unknown>';
+			const location = parts.pop();
+			const related = !file?.includes('node_modules/') && !file?.includes('node:internal/');
+
+			return {
+				location,
+				file: file.substring(file.indexOf('src/')),
+				related
+			};
+		})
+		.filter((l) => l.related)
+		.map((l) => `${l.location} at ${l.file}`);
+};
