8885c27c465f24f8a6c8471099d57cca7548ce03
SantioMC <business@santio.me>
2023-11-03 17:45:49 -0400

    refactor: Change user-controller to self-controller

---
+++ src/controllers/self-controller.ts
@@ -0,0 +1,17 @@
+import { Authorized, CurrentUser, Get, HttpError, JsonController } from 'routing-controllers';
+import { User } from '@clerk/clerk-sdk-node';
+import * as ServerManager from '@manager/server';
+
+@Authorized()
+@JsonController('/me')
+export class SelfController {
+	@Get('/servers')
+	async getServer(@CurrentUser() user: User) {
+		return (await ServerManager.getServers(user.id)).map((s) => {
+			return {
+				...ServerManager.scopeServer(s, [], user),
+				status: s.getStatus()
+			};
+		});
+	}
+}
